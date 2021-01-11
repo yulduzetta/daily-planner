165a60f28b9a8d0d3ec610478cda7a93d7c15975
yulduz <yulduz83@gmail.com>
2021-01-10 19:05:54 -0600

    updates timestamp and runs audit task every second

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,31 @@
 // If there is nothing in 'localStorage', sets the 'list' to an empty array
 var tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-// displays current day
-$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
-
-// color-flags the tasks based on the timestamp (past, present, future)
+// color-flag the tasks based on the timestamp (past, present, future)
 var auditTasks = function () {
   $(".hour").each(function (index) {
     var taskTimestampHr = moment($(this).text(), "h A");
     var nowTimestampHr = moment().hour("h A");
+    var taskRowEl = $(this).parent().children(".description");
+    
+    // removes all classes
+    taskRowEl.removeClass('future past present');
 
-    console.log(taskTimestampHr);
     if (taskTimestampHr.isAfter(nowTimestampHr)) {
-      // alert('FUTURE');
-      $(this).parent().children(".description").addClass("future");
-    }
-
-    else if (taskTimestampHr.isSame(nowTimestampHr, 'hour')){
-        $(this).parent().children('.description').addClass('present');
-    }
-    else {
-        $(this).parent().children('.description').addClass('past');
+      taskRowEl.addClass("future");
+    } else if (taskTimestampHr.isSame(nowTimestampHr, "hour")) {
+      taskRowEl.addClass("present");
+    } else {
+      taskRowEl.addClass("past");
     }
   });
 };
 
-// on page load
+// on page load populate tasks  description
 var loadTasks = function () {
   var taskDescriptionEntryEl = $(".row .description");
 
-  // reads tasks from local storage and displays on UI
+  // read tasks from local storage and display on UI
   if (tasks.length == 0) {
     for (var i = 0; i < taskDescriptionEntryEl.length; i++) {
       tasks.push("");
@@ -40,7 +36,25 @@ var loadTasks = function () {
       $(this).val(tasks[index]);
     });
   }
+  auditTasks();
 };
+// update current timestamp every second
+var periodicallyUpdateCurrentTimeStamp = function () {
+  setInterval(function () {
+    $("#currentDay").text(moment().format("h:mm:ss A dddd, MMMM Do, YYYY"));
+    auditTasks();
+  }, 1000);
+};
+
+// // when changes are unsaved, revert css styling of background and color
+// $(".row .description").on("blur", function () {
+//   location.reload();
+// });
+
+// on task row click, highlight the task
+$(".row .description").on("click select", function () {
+  $(this).css({ "background-color": "white", color: "black" });
+});
 
 // on task save, update the tasks object and the localStorage
 $(".row .saveBtn").on("click", function () {
@@ -50,7 +64,9 @@ $(".row .saveBtn").on("click", function () {
 
   tasks[taskId] = text;
   localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  location.reload();
 });
 
 loadTasks();
-auditTasks();
+periodicallyUpdateCurrentTimeStamp();
